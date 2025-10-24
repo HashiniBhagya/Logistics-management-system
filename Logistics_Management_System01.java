b7952a30a1d50d2a1b61f59fa92d91e5fd257669
HashiniBhagya <as20240973@sci.sjp.ac.lk>
2025-10-24 20:21:52 +0530

    2.Added arrays for data storage

--- Logistics_Management_System01.java
+++ Logistics_Management_System01.java
@@ -3,6 +3,10 @@ package com.mycompany.logistics_management_system01;
 
 
 public class Logistics_Management_System01 {
+    
+    static String[] cities = new String[30];
+    static int[][] distances = new int[30][30];
+    static int cityCount = 0;
 
     public static void main(String[] args) {
         System.out.println("=== Logistics Management System ===");
