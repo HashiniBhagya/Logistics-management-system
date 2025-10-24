db161d8721266bef22ce56bc1adba5092b658dbf
HashiniBhagya <as20240973@sci.sjp.ac.lk>
2025-10-24 19:36:10 +0530

    Add files via upload
     1.Created project structure and main class

---
+++ Logistics_Management_System01.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.logistics_management_system01;
+
+
+public class Logistics_Management_System01 {
+
+    public static void main(String[] args) {
+        System.out.println("=== Logistics Management System ===");
+    }
+}
